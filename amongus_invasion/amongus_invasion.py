694cbbd3580dd7632c0269c6bafae577c8c03bcb
ASUS <delenie1.3@gmail.com>
2021-10-11 14:36:00 +0500

    add class settings

--- amongus_invasion/amongus_invasion.py
+++ amongus_invasion/amongus_invasion.py
@@ -1,27 +1,28 @@
 import sys
 import pygame
 from os import path
-
-img_dir = path.join(path.dirname(__file__), 'img')#определение папки с изображениями
-
+from settings import Settings
 
 class AmongusInvasion:#класс для управления поведением игры и ресурсами
     def __init__(self):#инициализация игры и ресурсов
         pygame.init()
+        self.img_dir = path.join(path.dirname(__file__), 'img')#определение папки с изображениями
 
-        self.screen = pygame.display.set_mode((1000, 750))
+        self.settings = Settings()#экземпляр настроек
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))#размер окна
         pygame.display.set_caption('Among Us вторжение')
 
-        self.background = pygame.image.load(path.join(img_dir, 'sky.png')).convert()#путь к фону
+        self.background = pygame.image.load(path.join(self.img_dir, 'sky.png')).convert()#путь к фону
         self.background_rect = self.background.get_rect()#загрузка фона
-
+  
     def run_game(self):#запуск игры
         while True:#отслеживание клавиатуры и мыши
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill('BLACK')#цвет фона
+            #self.screen.fill('RED')#цвет фона
             self.screen.blit(self.background, self.background_rect)#наложение фона
             #all_sprites.draw(self.screen)
 
---
+++ amongus_invasion/settings.py
@@ -0,0 +1,7 @@
+class Settings():#класс настроек игры
+    def __init__(self):#иницализация настроек
+
+        #параметры экрана
+        self.screen_width = 1000
+        self.screen_height = 750
+        
